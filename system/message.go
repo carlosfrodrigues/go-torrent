2eea53fcd3ad90375fb75c1ac09fd403f6bc327d
Carlos Felix <cj.felixrodrigues@gmail.com>
2020-08-06 02:18:14 -0300

    Update message.go

--- system/message.go
+++ system/message.go
@@ -24,41 +24,41 @@ type Message struct {
 }
 
 func (m *Message) GenerateMessage() []byte {
-    if m == nil {
-        return make([]byte, 4)
-    }
-    length := uint32(len(m.Payload) + 1)
-    msg := make([]byte, 4+length)
-    binary.BigEndian.PutUint32(msg[0:4], length)
-    msg[4] = byte(m.ID)
-    copy(msg[5:], m.Payload)
-    return msg
+	if m == nil {
+		return make([]byte, 4)
+	}
+	length := uint32(len(m.Payload) + 1)
+	msg := make([]byte, 4+length)
+	binary.BigEndian.PutUint32(msg[0:4], length)
+	msg[4] = byte(m.ID)
+	copy(msg[5:], m.Payload)
+	return msg
 }
 
 func ReadMessage(r io.Reader) (*Message, error) {
-    lengthBuf := make([]byte, 4)
-    _, err := io.ReadFull(r, lengthBuf)
-    if err != nil {
-        return nil, err
-    }
-    length := binary.BigEndian.Uint32(lengthBuf)
+	lengthBuf := make([]byte, 4)
+	_, err := io.ReadFull(r, lengthBuf)
+	if err != nil {
+		return nil, err
+	}
+	length := binary.BigEndian.Uint32(lengthBuf)
 
-    if length == 0 {
-        return nil, nil
-    }
+	if length == 0 {
+		return nil, nil
+	}
 
-    messageBuf := make([]byte, length)
-    _, err = io.ReadFull(r, messageBuf)
-    if err != nil {
-        return nil, err
-    }
+	messageBuf := make([]byte, length)
+	_, err = io.ReadFull(r, messageBuf)
+	if err != nil {
+		return nil, err
+	}
 
-    m := Message{
-        ID:      messageID(messageBuf[0]),
-        Payload: messageBuf[1:],
-    }
+	m := Message{
+		ID:      messageID(messageBuf[0]),
+		Payload: messageBuf[1:],
+	}
 
-    return &m, nil
+	return &m, nil
 }
 
 func FormatRequest(index, begin, length int) *Message {
